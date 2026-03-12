396490442de694f73d7b1b217d977b3ed0617927
Kyle <kyle@hdu.edu.cn>
2026-03-12 20:17:18 +0800

    feat: 添加 `mount` (40) 和 `umount` (39) syscall。
    这一个commit把设备也抽象成文件，形成 `devfs`，挂载到 `/dev` 下。同时，发现之前的 `execve` 放置指针的方式不符合C ABI，已修正。
    
    为了测试它，需要给qemu添加第二块磁盘，见Makefile。对于多块磁盘，系统挂载顺序是 `sda`、`sda2`...此处认为它是多块磁盘，而不是同一磁盘的多个分区。

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -20,10 +20,10 @@ pub const SYSCALL_MKDIRAT: usize = 34;
 pub const SYSCALL_UNLINKAT: usize = 35;
 /// linkat syscall
 pub const SYSCALL_LINKAT: usize = 37;
+/// umount syscall
+pub const SYSCALL_UMOUNT: usize = 39;
 /// mount syscall
 pub const SYSCALL_MOUNT: usize = 40;
-/// umount syscall
-pub const SYSCALL_UMOUNT: usize = 41;
 /// chdir syscall
 pub const SYSCALL_CHDIR: usize = 49;
 /// openat syscall
@@ -165,6 +165,7 @@ pub fn syscall(syscall_id: usize, args: [usize; 6]) -> isize {
             args[3] as *const u8,
             args[4] as u32,
         ),
+        SYSCALL_UMOUNT => sys_umount(args[0] as *const u8, args[1]),
         SYSCALL_MOUNT => sys_mount(
             args[0] as *const u8,
             args[1] as *const u8,
@@ -172,7 +173,6 @@ pub fn syscall(syscall_id: usize, args: [usize; 6]) -> isize {
             args[3],
             args[4] as *const u8,
         ),
-        SYSCALL_UMOUNT => sys_umount(args[0] as *const u8, args[1] as u32),
         SYSCALL_OPENAT => sys_open(args[0] as isize, args[1] as *const u8, args[2] as i32, args[3] as u32),
         SYSCALL_CLOSE => sys_close(args[0] as u32),
         SYSCALL_PIPE2 => sys_pipe2(args[0] as *mut i32, args[1] as i32),
