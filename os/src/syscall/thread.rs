98de48cdb0cf57c6440a49ead23d6d87dd3b8a8a
Ga1axy_Mac <Jimli176017@gmail.com>
2026-04-28 20:46:48 +0800

    refactor: 解耦任务创建和进入调度器

--- os/src/syscall/thread.rs
+++ os/src/syscall/thread.rs
@@ -1,7 +1,8 @@
 use crate::{
     mm::kernel_token,
+    sched::activate_task,
     syscall::errno::ERRNO,
-    task::{add_task, current_task, TaskControlBlock},
+    task::current_task,
     trap::{trap_handler, TrapContext},
 };
 use alloc::sync::Arc;
@@ -28,28 +29,14 @@ pub fn sys_thread_create(entry: usize, arg: usize) -> isize {
         )
     };
     // create a new thread
-    let new_task = Arc::new(TaskControlBlock::new(
-        Arc::clone(&process),
-        ustack_base,
-        true,
-        sched_attr,
-    ));
+    let new_task = process.create_task(ustack_base, true, sched_attr);
     {
         let affinity_mask = task.inner_exclusive_access().sched.cpu_affinity_mask;
         new_task.inner_exclusive_access().sched.cpu_affinity_mask = affinity_mask;
     }
-    // add new task to scheduler
-    add_task(Arc::clone(&new_task));
     let new_task_inner = new_task.inner_exclusive_access();
     let new_task_res = new_task_inner.res.as_ref().unwrap();
     let new_task_tid = new_task_res.tid;
-    let mut process_inner = process.inner_exclusive_access();
-    // add new thread to current process
-    let tasks = &mut process_inner.tasks;
-    while tasks.len() < new_task_tid + 1 {
-        tasks.push(None);
-    }
-    tasks[new_task_tid] = Some(Arc::clone(&new_task));
     let new_task_trap_cx = new_task_inner.get_trap_cx();
     *new_task_trap_cx = TrapContext::app_init_context(
         entry,
@@ -59,6 +46,9 @@ pub fn sys_thread_create(entry: usize, arg: usize) -> isize {
         trap_handler as usize,
     );
     (*new_task_trap_cx).x[10] = arg;
+    drop(new_task_inner);
+    process.attach_task(Arc::clone(&new_task));
+    activate_task(new_task);
     new_task_tid as isize
 }
 /// get current thread id syscall
