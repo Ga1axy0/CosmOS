e74af70a078755380bd6c74b9da27e7c9d0773ba
Kyle <kyle@hdu.edu.cn>
2026-05-03 21:38:55 +0800

    refactor: 把frame allocator换成基于buddy的分配器。之所以不复用`buddy_system_allocator` crate，因为它会依赖于底层的frame allocator。
    至此，heap实现动态增长，显著节省了内存。
    
    TODO:
    1) 堆增长了frame之后，还不能缩减。实现这个可能手动实现（换掉）`buddy_system_allocator` crate。
    2) `frame_alloc_contiguous` 对于过度的对齐可能会消极返回None。

--- os/src/config.rs
+++ os/src/config.rs
@@ -7,7 +7,9 @@ pub const USER_STACK_SIZE: usize = 1024 * 16; // 16 KiB
 /// kernel stack size
 pub const KERNEL_STACK_SIZE: usize = 4096 * 4;
 /// kernel heap size
-pub const KERNEL_HEAP_SIZE: usize = 0x400_0000;
+pub const MAX_KERNEL_HEAP_SIZE: usize = 0x400_0000;
+/// base address of the dynamically mapped kernel heap window
+pub const KERNEL_HEAP_BASE: usize = 0xffff_ffc0_0000_0000;
 /// max harts reserved by the kernel SMP bootstrap path
 pub const MAX_HARTS: usize = 8;
 /// physical memory end address
